90ca126b07f629afc9b63276484de10c365dc7e3
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-12-04 18:45:25 -0500

    added animations to EventfulComponent and Header

--- src/app/components/EventfulComponent.jsx
+++ src/app/components/EventfulComponent.jsx
@@ -3,38 +3,61 @@
 
 import { React, useState } from 'react';
 import styles from './EventfulComponent.module.css'
+import { motion } from 'framer-motion';
 
 export default function EventfulComponent() {
   const [enable, setEnable] = useState(false) 
 
+  const allVariants = {
+    offScreen: { y: 50, opacity: 0 },
+    onScreen: { y: 0, opacity: 1, transition: { duration: .5 } }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.image_container}>
-        <img className={styles.image} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img2.png"></img>
+        <motion.img
+        initial={{ y: 50 }}
+        whileInView={{ y: 0, transition: { duration: .5} }} 
+          className={styles.image} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img2.png">
+        </motion.img>
         <img className={styles.blue_stripe} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe2.png"></img>
       </div>
       <div className={styles.text_container}>
-        <div className={styles.title_container}>
+        <motion.div 
+          className={styles.title_container}
+          variants = {allVariants}
+          initial = 'offScreen'
+          whileInView='onScreen'>
           <div className={styles.title_1}>
             we&rsquo;re
           </div>
           <div className={styles.title_2}>
             eventful
           </div>
-        </div>
-        <div className={styles.subtitle_container}>
+        </motion.div>
+        <motion.div 
+        className={styles.subtitle_container}
+        variants = {allVariants}
+        initial = 'offScreen'
+        whileInView='onScreen'
+        >
           <div className={styles.subtitle_1}>
             visual experiences for our
           </div>
           <div className={styles.subtitle_2}>
             businesses and brands
           </div>
-        </div>
-        <div className={styles.button_container}>
+        </motion.div>
+        <motion.div 
+          className={styles.button_container}
+          variants = {allVariants}
+          initial = 'offScreen'
+          whileInView='onScreen'>
           {!enable ? <div className={styles.view_button}>
             <img src="https://d2mf4l4ba7pnlp.cloudfront.net/images/viewit.svg"></img>
           </div> : null}
-        </div>
+        </motion.div>
       </div>
     </div>
   )
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,25 +1,66 @@
+'use client'
+
 import React from "react"
 import Head from "next/head";
+import { motion } from "framer-motion"
 import styles from './Header.module.css';
 
 // header should include thin orange bar, image on the left, and navbar icon on the right
 export default function Header() {
+    const barVariants = {
+        hidden : { opacity: 0, y: -20},
+        visible : { opacity: 1, 
+                    y: 0,
+                    transition: { 
+                        delay: 0, 
+                        duration: .3, // Increase duration to slow down the animation
+                    }},
+    }
+
+    const iconVariants = {
+        hidden: { opacity: 0 },
+        visible: (custom) => (
+            { opacity: 1,
+                transition: { delay: custom * 0.3, duration: .3 }}),
+        hover: {
+            filter: 'brightness(0) saturate(100%) invert(37%) sepia(100%) saturate(4250%) hue-rotate(14deg) brightness(102%) contrast(101%)',
+            }
+    }
+
     return (
         <>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <div>
-            <div className={styles.fixed_bar}>
-                <div className={styles.fixed_orange_bar}></div> {/*This is the orange bar*/}
-            </div>
+            <motion.div 
+                className={styles.fixed_bar}
+                variants={barVariants}
+                initial="hidden"
+                animate="visible">
+            </motion.div> {/*This is the orange bar*/}
             <div className={styles.fixed_container}>
-                <img className={styles.nbcu_logo} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/verticallogo.svg"></img>
-                <div className={styles.navbar_icon}> {/*This is the navbar icon*/}
+                <motion.img 
+                    className={styles.nbcu_logo} 
+                    src="https://d2mf4l4ba7pnlp.cloudfront.net/images/verticallogo.svg"
+                    variants={iconVariants}
+                    initial="hidden"
+                    animate="visible"
+                    whileHover='hover'
+                    custom={1}
+                    >
+                </motion.img>
+                <motion.div 
+                    className={styles.navbar_icon}
+                    variants={iconVariants}
+                    initial="hidden"
+                    animate="visible"
+                    whileHover='hover'
+                    custom={2}> {/*This is the navbar icon*/}
                     <span className={styles.menu_line}></span>
                     <span className={styles.menu_line}></span>
                     <span className={styles.menu_line}></span>
-                </div> 
+                </motion.div> 
             </div>
         </div>
         </>
