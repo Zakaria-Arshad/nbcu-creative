670bf6bfdef3aa0f128d215865b14ad6492dc644
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-12-04 19:06:14 -0500

    Add animations for BigFans and Thinkers Components

--- src/app/components/BigFansComponent.jsx
+++ src/app/components/BigFansComponent.jsx
@@ -3,44 +3,67 @@
 
 import { React, useState } from 'react';
 import styles from './BigFansComponent.module.css'
-// src="https://d2mf4l4ba7pnlp.cloudfront.net/images/blacklistheader.png" for background
-// src="src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"" for blue stripe
-// make a full container for the background
-// then put the components inside of it
+import { motion } from 'framer-motion';
 
-// to add in the future: conditional rendering for certain components and a prop for rendering animation
 export default function BigFansComponent() {
   const [enable, setEnable] = useState(false) 
 
+  const allVariants = {
+    offScreen: { y: 50, opacity: 0 },
+    onScreen: { y: 0, opacity: 1, transition: { duration: 0.5 } }
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.image_container}>
-        <img className={styles.image} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/blacklistheader.png"></img>
-        <img className={styles.blue_stripe} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"></img>
-      </div>
-      <div className={styles.text_container}>
-        <div className={styles.title_container}>
-          <div className={styles.title_1}>
-            we&rsquo;re
-          </div>
-          <div className={styles.title_2}>
-            big fans
-          </div>
-        </div>
-        <div className={styles.subtitle_container}>
-          <div className={styles.subtitle_1}>
-            bringing our A-game to sports
-          </div>
-          <div className={styles.subtitle_2}>
-            and entertainment brands
-          </div>
-        </div>
-        <div className={styles.button_container}>
-          {!enable ? <div className={styles.view_button}>
-            <img src="https://d2mf4l4ba7pnlp.cloudfront.net/images/viewit.svg"></img>
-          </div> : null}
-        </div>
-      </div>
+    <div className={styles.image_container}>
+    <motion.img 
+        className={styles.image} 
+        initial={{ y: 50 }}
+        whileInView={{ y: 0, transition: { duration: 0.5 } }}
+        src="https://d2mf4l4ba7pnlp.cloudfront.net/images/blacklistheader.png">
+    </motion.img>
+      <img className={styles.blue_stripe} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"></img>
     </div>
+    <div className={styles.text_container}>
+      <motion.div 
+        className={styles.title_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        <div className={styles.title_1}>
+          we&rsquo;re
+        </div>
+        <div className={styles.title_2}>
+          big fans
+        </div>
+      </motion.div>
+      <motion.div 
+        className={styles.subtitle_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        <div className={styles.subtitle_1}>
+          bringing our A-game to sports
+        </div>
+        <div className={styles.subtitle_2}>
+          and entertainment brands
+        </div>
+      </motion.div>
+      <motion.div 
+        className={styles.button_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        {!enable ? (
+          <div className={styles.view_button}>
+            <img src="https://d2mf4l4ba7pnlp.cloudfront.net/images/viewit.svg"></img>
+          </div>
+        ) : null}
+      </motion.div>
+    </div>
+  </div>
   )
 }
--- src/app/components/ThinkersComponent.jsx
+++ src/app/components/ThinkersComponent.jsx
@@ -3,48 +3,69 @@
 
 import { React, useState } from 'react';
 import styles from './ThinkersComponent.module.css'
+import { motion } from 'framer-motion';
 
-// src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img3.png" for background
-// src="src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"" for blue stripe
-// make a full container for the background
-// then put the components inside of it
-
-// to add in the future: conditional rendering for certain components and a prop for rendering animation
 export default function ThinkersComponent() {
   const [enable, setEnable] = useState(false) 
+  const allVariants = {
+    offScreen: { y: 50, opacity: 0 },
+    onScreen: { y: 0, opacity: 1, transition: { duration: .5 } }
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.image_container}>
-        <img className={styles.image} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img3.png"></img>
-        <img className={styles.blue_stripe} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"></img>
-      </div>
-      <div className={styles.text_container}>
-        <div className={styles.title_container}>
-          <div className={styles.title_1}>
-            we&rsquo;re
-          </div>
-          <div className={styles.title_2}>
-            thinkers
-          </div>
-          <div className={styles.title_3}>
-            who do
-          </div>
-        </div>
-        <div className={styles.subtitle_container}>
-          <div className={styles.subtitle_1}>
-            building brand identity
-          </div>
-          <div className={styles.subtitle_2}>
-            from concept to launch
-          </div>
-        </div>
-        <div className={styles.button_container}>
-          {!enable ? <div className={styles.view_button}>
-            <img src="https://d2mf4l4ba7pnlp.cloudfront.net/images/viewit.svg"></img>
-          </div> : null}
-        </div>
-      </div>
+    <div className={styles.image_container}>
+      <motion.img 
+        className={styles.image} 
+        initial={{ y: 50 }}
+        whileInView={{ y: 0, transition: { duration: 0.5 } }}
+        src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img3.png">
+      </motion.img>
+      <img className={styles.blue_stripe} src="https://d2mf4l4ba7pnlp.cloudfront.net/images/bluestripe.png"></img>
     </div>
+    <div className={styles.text_container}>
+      <motion.div 
+        className={styles.title_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        <div className={styles.title_1}>
+          we&rsquo;re
+        </div>
+        <div className={styles.title_2}>
+          thinkers
+        </div>
+        <div className={styles.title_3}>
+          who do
+        </div>
+      </motion.div>
+      <motion.div 
+        className={styles.subtitle_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        <div className={styles.subtitle_1}>
+          building brand identity
+        </div>
+        <div className={styles.subtitle_2}>
+          from concept to launch
+        </div>
+      </motion.div>
+      <motion.div 
+        className={styles.button_container}
+        variants={allVariants}
+        initial="offScreen"
+        whileInView="onScreen"
+      >
+        {!enable ? (
+          <div className={styles.view_button}>
+            <img src="https://d2mf4l4ba7pnlp.cloudfront.net/images/viewit.svg"></img>
+          </div>
+        ) : null}
+      </motion.div>
+    </div>
+  </div>
   )
 }
