a1705a3433d3091e16fd1b2ed2c6d2e7fe829aee
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-12-06 09:53:17 -0500

    added animations to LoveIdeasMakingThings.jsx

--- src/app/components/LoveIdeasMakeThings.jsx
+++ src/app/components/LoveIdeasMakeThings.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
-import styles from './LoveIdeasMakeThings.module.css'
+import { motion } from 'framer-motion';
+import styles from './LoveIdeasMakeThings.module.css';
 
 export default function LoveIdeasMakeThings() {
+    // Variants for text animation
+    const textVariants = {
+        offScreen: { y: 50, opacity: 0 },
+        onScreen: { y: 0, opacity: 1, transition: {duration: 1} }
+    };
+
+    // Variants for line animation
+    const lineVariants = {
+        offScreen: { width: '0%' },
+        onScreen: { width: '35%',  transition: { duration: 1 }}
+    }
 
     return (
         <div className={styles.container}>
-            <div className={styles.text}>
+            <motion.div 
+                className={styles.text}
+                initial="offScreen"
+                whileInView="onScreen"
+                variants={textVariants}
+            >
                 we love ideas.
-            </div>
+            </motion.div>
             <br />
-            <hr className={styles.line}></hr>
-            <div className={styles.text}>
+            <motion.hr 
+                className={styles.line}
+                initial="offScreen"
+                whileInView="onScreen"
+                variants={lineVariants}
+            />
+            <motion.div 
+                className={styles.text}
+                initial="offScreen"
+                whileInView="onScreen"
+                variants={textVariants}
+            >
                 we make things.
-            </div>
+            </motion.div>
         </div>
     );
 };
+
 
