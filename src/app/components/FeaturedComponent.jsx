eceb46082415935645b0cbdcb9de31efb66b949e
Zakaria Arshad <zakaria.arshad@nyu.edu>
2024-01-05 14:15:55 -0500

    built basic implementation of FeaturedComponent

---
+++ src/app/components/FeaturedComponent.jsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import styles from '../css-styles/FeaturedComponent.module.css'
+
+export default function FeaturedComponent( {images, titles} ) {
+
+    return (
+        <div className={styles.container}>
+            {images.map((image, index) => (
+                <motion.div className={styles.box} key={index} initial={{ y: 50 }}
+                whileInView={{ y: 0, transition: { duration: .7} }} >
+                    <img className={styles.image} src={image}>
+                    </img>
+                    <div className={styles.overlay}>
+                        <div className={styles.overlay_content}>{titles[index]}</div>
+                    </div>
+                </motion.div>
+            ))}
+        </div>
+    )
+}
--- src/app/freshbaked/page.jsx
+++ src/app/freshbaked/page.jsx
@@ -1,13 +1,18 @@
 import { React } from 'react';
 import Header from '../components/Header'
 import FreshBakedComponent from '../components/FreshBakedComponent'
+import FeaturedComponent from '../components/FeaturedComponent';
 import Footer from '../components/Footer'
 
 export default function FreshBaked() {
+    // would get these from API
+    const images = ["https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/blacklist-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/treeLighting-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/Symphony-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/ThankYou-Tile.jpg"]
+    const titles = ["The Blacklist", "Christmas in Rockefeller Center", "Symphony", "Jeff Shell Town Hall: A Thank You"]
     return (
         <>
         <Header />
         <FreshBakedComponent />
+        <FeaturedComponent images={images} titles={titles}/>
         <Footer />  
         </>
 
