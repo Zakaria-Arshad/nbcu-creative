31cea66df3d3403e99dcc2a03723478c96af20e9
Zakaria Arshad <zakaria.arshad@nyu.edu>
2024-01-05 12:26:15 -0500

    added FreshBakedComponent

---
+++ src/app/components/FreshBakedComponent.jsx
@@ -0,0 +1,49 @@
+'use client'
+import React from 'react';
+import styles from '../css-styles/FreshBakedComponent.module.css';
+import { motion } from 'framer-motion';
+
+export default function FreshBakedComponent() {
+    const allVariants = {
+        offScreen: { y: 50, opacity: 0 },
+        onScreen: { y: 0, opacity: 1, transition: { duration: .5 } }
+      }
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.image_container}>
+                <motion.img 
+                initial={{ y: 50 }}
+                whileInView={{ y: 0, transition: { duration: .5} }} 
+                src="https://d2mf4l4ba7pnlp.cloudfront.net/images/img5.png"
+                className={styles.image}
+                >
+                </motion.img>
+            </div>
+            <div className={styles.text_container}>
+                <motion.div 
+                className={styles.title_container}
+                variants = {allVariants}
+                initial = 'offScreen'
+                whileInView='onScreen'>
+                <div className={styles.title}>
+                    fresh baked
+                </div>
+                </motion.div>
+                <motion.div 
+                className={styles.subtitle_container}
+                variants = {allVariants}
+                initial = 'offScreen'
+                whileInView='onScreen'
+                >
+                <div className={styles.subtitle_1}>
+                    what&rsquo;s new and trending
+                </div>
+                <div className={styles.subtitle_2}>
+                    here at the shop
+                </div>
+                </motion.div>
+            </div>
+        </div>
+    );
+};
--- src/app/freshbaked/page.jsx
+++ src/app/freshbaked/page.jsx
@@ -1,13 +1,15 @@
 import { React } from 'react';
 import Header from '../components/Header'
+import FreshBakedComponent from '../components/FreshBakedComponent'
 import Footer from '../components/Footer'
 
 export default function FreshBaked() {
     return (
         <>
         <Header />
-        <h1>FreshBaked</h1>
+        <FreshBakedComponent />
         <Footer />  
         </>
+
     )
 }
