b4afd4d231b86a71d0b9c23276dba9f3458d58c1
Zakaria Arshad <zakaria.arshad@nyu.edu>
2024-01-05 14:42:42 -0500

    added FeaturedComponent to all subpages

--- src/app/connected/page.jsx
+++ src/app/connected/page.jsx
@@ -1,13 +1,17 @@
 import { React } from 'react';
 import Header from '../components/Header'
 import ConnectedComponent from '../components/ConnectedComponent';
+import FeaturedComponent from '../components/FeaturedComponent';
 import Footer from '../components/Footer'
 
 export default function Connected() {
+    const images = ["https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/StayHealthy-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/MeetTheMoment-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/GoalModels-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/ComcastNBCUnites-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/PhishDay-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/TheSecretLifeOfPets-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/Animation-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/TODASTWD-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/BYPTWD-Tile.jpg"]
+    const titles = ["Stay Healthy Work Safe", "Meet the Moment", "#GoalModels", "Comcast NBCUnites", "Phish Day", "The Secret Life of Pets", "Animation", "Take Our Daughters & Sons To Work Day", "Bring Your Parents To Work Day"]
     return (
         <>
         <Header />
         <ConnectedComponent enable={false}/>
+        <FeaturedComponent images={images} titles={titles}/>
         <Footer />  
         </>
     )
--- src/app/fans/page.jsx
+++ src/app/fans/page.jsx
@@ -1,13 +1,17 @@
 import { React } from 'react';
 import Header from '../components/Header'
 import BigFansComponent from '../components/BigFansComponent';
+import FeaturedComponent from '../components/FeaturedComponent';
 import Footer from '../components/Footer'
 
 export default function Fans() {
+    const images = ["https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/blacklist-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/buble-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/oly-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/2018WorldCup-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/SuperBowl-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/Rio-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/Sochi-Tile.jpg", "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/London-Tile.jpg"]
+    const titles = ["The Blacklist", "Michael Buble's Christmas in the City", "PyeongChang Olympics", "2018 World Cup", "Super Bowl 50", "Rio Olympics", "Sochi Olympics", "London Olympics"]
     return (
         <>
         <Header />
         <BigFansComponent enable={false}/>
+        <FeaturedComponent images={images} titles={titles}/>
         <Footer />  
         </>
     )
