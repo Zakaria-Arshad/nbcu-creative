a4fe0eb4e9f578e92f571f036586cc898ff04516
Zakaria Arshad <zakaria.arshad@nyu.edu>
2024-02-14 12:42:41 -0500

    adding error catching for FreshBaked due to API error

--- src/app/fresh-baked/page.jsx
+++ src/app/fresh-baked/page.jsx
@@ -7,12 +7,12 @@ import { convertFooterHTMLToReact } from "../utils/footerapi";
 import { parseImageGridData } from "../utils/imagegridapi";
 
 async function getData() { // get all images
-  const res = await fetch(process.env.BASE_API_URL, { cache: "force-cache" })
+  const res = await fetch(process.env.BASE_API_URL)
   const data = await res.json()
 
-  const blockRes = await fetch (`${process.env.BASE_API_URL_2}=fresh-baked`, { cache: "force-cache" })
+  const blockRes = await fetch (`${process.env.BASE_API_URL_2}=fresh-baked`)
   const blockData = await blockRes.json()
-  const featuredArray = parseImageGridData(blockData.data.contents)
+  const featuredArray = blockData.status !== 400 ? parseImageGridData(blockData.data.contents) : [];
 
   const FooterData = convertFooterHTMLToReact(data[1].data.footer)
   return [featuredArray, FooterData];
@@ -26,19 +26,6 @@ export const metadata = {
 
 
 export default async function FreshBaked() {
-  // would get these from API
-  const images = [
-    "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/blacklist-Tile.jpg",
-    "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/treeLighting-Tile.jpg",
-    "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/Symphony-Tile.jpg",
-    "https://d2mf4l4ba7pnlp.cloudfront.net/images/detailpages/ThankYou-Tile.jpg",
-  ];
-  const titles = [
-    "The Blacklist",
-    "Christmas in Rockefeller Center",
-    "Symphony",
-    "Jeff Shell Town Hall: A Thank You",
-  ];
 
   const data = await getData();
   const featuredArray = data[0];
